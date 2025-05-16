813df7b650d0b96da84ed3c3e281c4427731f017
Akhilesh Joshi <theakhileshjoshi@gmail.com>
2025-05-16 23:55:53 +0530

    Removing Git Keep

--- create-git-keep.cmd
+++ create-git-keep.cmd
@@ -1,8 +1,5 @@
 @echo off
-for /f "delims=" %%d in ('dir /ad /b /s') do (
-    dir /a /b "%%d" | findstr . >nul
-    if errorlevel 1 (
-        echo Creating .gitkeep in %%d
-        echo. > "%%d\.gitkeep"
-    )
+for /r %%f in (.gitkeep) do (
+    echo Deleting %%f
+    del /f /q "%%f"
 )
